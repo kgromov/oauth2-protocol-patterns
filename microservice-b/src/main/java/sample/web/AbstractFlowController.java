f60fa5e53f9e5ec6bd4f60c745f12798d534a904
Joe Grandja <jgrandja@vmware.com>
2021-01-26 14:34:48 -0500

    Remove security in microservice-b

--- microservice-b/src/main/java/sample/web/AbstractFlowController.java
+++ microservice-b/src/main/java/sample/web/AbstractFlowController.java
@@ -15,15 +15,17 @@
  */
 package sample.web;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import sample.config.ServicesConfig;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.reactive.function.client.WebClient;
-import sample.config.ServicesConfig;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
@@ -73,11 +75,13 @@ abstract class AbstractFlowController {
 		ServiceCallResponse serviceCallResponse = new ServiceCallResponse();
 		serviceCallResponse.setServiceName(SERVICE_B);
 		serviceCallResponse.setServiceUri(request.getRequestURL().toString());
-		serviceCallResponse.setJti(jwtAuthentication.getToken().getId());
-		serviceCallResponse.setSub(jwtAuthentication.getToken().getSubject());
-		serviceCallResponse.setAud(jwtAuthentication.getToken().getAudience());
-		serviceCallResponse.setAuthorities(jwtAuthentication.getAuthorities().stream()
-				.map(GrantedAuthority::getAuthority).sorted().collect(Collectors.toList()));
+		if (jwtAuthentication != null) {
+			serviceCallResponse.setJti(jwtAuthentication.getToken().getId());
+			serviceCallResponse.setSub(jwtAuthentication.getToken().getSubject());
+			serviceCallResponse.setAud(jwtAuthentication.getToken().getAudience());
+			serviceCallResponse.setAuthorities(jwtAuthentication.getAuthorities().stream()
+					.map(GrantedAuthority::getAuthority).sorted().collect(Collectors.toList()));
+		}
 		if (serviceCallResponses != null) {
 			serviceCallResponse.setServiceCallResponses(Arrays.asList(serviceCallResponses));
 		}
